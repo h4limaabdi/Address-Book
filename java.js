3116016d5d83ee586e27399f0bdb9e49f094a548
h4lima <h4limaabdi@gmail.com>
2025-10-06 07:50:56 +0300

    Have an array to contain all contact objects

--- java.js
+++ java.js
@@ -3,3 +3,17 @@ const firstNameInput = document.getElementById('firstName');
 const lastNameInput = document.getElementById('lastName');
 const phoneInput = document.getElementById('phoneNumber');
 const outputDiv = document.getElementById('output');
+
+const addressBook = [];
+
+function Contact(firstName, lastName, phoneNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+}
+
+function addContact(firstName, lastName, phoneNumber){
+    const contact = new Contact(firstName,lastName, phoneNumber);
+    addressBook.push(contact);
+    displayContacts();
+}
