d4ce775d9e040b94d8ff4393caa957da24dd60b7
h4lima <h4limaabdi@gmail.com>
2025-10-06 07:49:21 +0300

    use JavaScript to get element by id.

---
+++ java.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('contactForm');
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const phoneInput = document.getElementById('phoneNumber');
+const outputDiv = document.getElementById('output');
